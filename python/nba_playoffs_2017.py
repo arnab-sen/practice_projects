6f74633aeb3048a7563326ea02641f69fd34ff0d
DESKTOP-IVA7AI6\Arnab <azure3317@gmail.com>
2017-04-16 18:34:23 +1200

    Now obtains all team names and scores correctly; TODO: organise team and scores into matches and level (i.e. is it in the First Round,  Conference Semifinals,  Conference Finals, or the  NBA Finals)

--- python/nba_playoffs_2017.py
+++ python/nba_playoffs_2017.py
@@ -3,7 +3,7 @@ This program obtains the results of the NBA Playoffs from Wikipedia,
 namely the data from the Bracket section.
 """
 import wikipedia
-def get_results(html_section, team_key):
+def get_results(html_section, team_key, colour_key):
     # Get team name
     start = html_section.find(team_key) + len(team_key)
     html_section = html_section[start + 1:]
@@ -17,17 +17,18 @@ def get_results(html_section, team_key):
     # Get team series score e.g. Cleveland Cavaliers 4
     # Use the background colour of the bracket box
     # as the key to find the score
-    colour_key = "#87cefa"
     score_start = html_section.find(colour_key)
-    html_section = html_section[score_start + 1:]
-    print(html_section)
-    score_slice = html_section[score_start : html_section.find("</td>")]
-    #print(score_slice)
-    #score = html_section[score_start]
-    html_section = html_section[score_start:]
+    html_section = html_section[score_start + len(colour_key) + 4:]
+    #print(html_section)
+    if html_section[0] == "<":
+        score = html_section[3]
+        html_section = html_section[4:]
+    else:
+        score = html_section[0]
+        html_section = html_section[1:]
+    #print(score)
 
-    #return [team_name, score, html_section]
-    return [""]
+    return [team_name, score], html_section
 
 data = []
 playoffs_2017 = wikipedia.page("2017 NBA Playoffs")
@@ -38,14 +39,26 @@ page_html = playoffs_2016.html()
 team_key = "2015%E2%80%9316"
 html_section = page_html[page_html.find(team_key):]
 
-get_results(html_section, team_key)
+#get_results(html_section, team_key, colour_key)
 
 # data is a list of lists
-#data += [get_results(html_section, team_key)]
-#data += [get_results(data[0][2], team_key)]
+# Get Eastern Conference results
+colour_key = "#87cefa"
+for i in range(15):
+    d, html_section = get_results(html_section, team_key, colour_key)
+    data += [d]
+
+# Get Western Conference results
+colour_key = "#ffaeb9"
+for i in range(15):
+    d, html_section = get_results(html_section, team_key, colour_key)
+    data += [d]
 
 #print(data[0][0], data[0][1])
 #print(data[1][0], data[1][1])
+for i in data: 
+    print(i)
+    #pass
 
 
 #print(html_section)
