ea821853adfff3bdfc777a443eda333ef606206c
DESKTOP-IVA7AI6\Arnab <azure3317@gmail.com>
2017-04-16 23:56:50 +1200

    Data extraction functions rewritten; html now split and parsed into a list; data extraction functions now handle empty bracket elements (i.e. the TBD entries for playoffs currently in progress, aka the current 2017 one)

--- python/nba_playoffs_2017.py
+++ python/nba_playoffs_2017.py
@@ -4,56 +4,6 @@ namely the data from the Bracket section.
 """
 import wikipedia
 
-def match_is_empty(html_section, team_key, colour_key):
-    if colour_key == "#87cefa":
-        check_key = "#87cefa;\"> &#160;<"
-    elif colour_key == "#ffaeb9":
-        check_key = "#ffaeb9\"> &#160;<"
-    start = html_section.find(colour_key)
-    html_section = html_section[start:]
-    team_name = html_section
-    print(team_name[:18], "vs", check_key)
-    if team_name[:18] != check_key:
-        html_section = html_section[start + 1:]
-        start = start = html_section.find(colour_key)
-        html_section = html_section[start + 1:]
-        return True, html_section
-    else: return False, html_section
-    #print(team_name[:17])
-    #print()
-    
-    
-
-def get_results(html_section, team_key, colour_key):
-    is_empty, html_section = match_is_empty(html_section, team_key, colour_key)
-    if is_empty:
-        return ["Team TBD", "Score TBD"], html_section
-    # Get team name
-    start = html_section.find(team_key) + len(team_key)
-    html_section = html_section[start + 1:]
-    team_name = html_section
-    underscores = [team_name.find("_"), ""]
-    t = team_name[underscores[0] + 1:]
-    underscores[1] = underscores[0] + t.find("_")
-    team_name = team_name[:underscores[0]] + \
-                " " + team_name[underscores[0] + 1: underscores[1] + 1]
-
-    # Get team series score e.g. Cleveland Cavaliers 4
-    # Use the background colour of the bracket box
-    # as the key to find the score
-    score_start = html_section.find(colour_key)
-    html_section = html_section[score_start + len(colour_key) + 4:]
-    #print(html_section)
-    if html_section[0] == "<":
-        score = html_section[3]
-        html_section = html_section[4:]
-    else:
-        score = html_section[0]
-        html_section = html_section[1:]
-    #print(score)
-
-    return [team_name, score], html_section
-
 def organise_results(data):
     first_round = [""] * 8
     conf_semi = [""] * 4
@@ -94,48 +44,97 @@ def organise_results(data):
         for i in conf_finals: print(i)
         print("\nNBA Finals:")
         print(nba_finals)
-  
+
+def get_results(html_list, team_key, colour_key):
+    # This function will extract match results
+    # and return the html_list with those
+    # results removed
+    html_list[0] = "Empty element!"
+    # Get team name   
+    for i in range(len(html_list)):
+        #print(html_list[i])
+        if colour_key in html_list[i]:            
+            index = i
+            break
+        else:
+            index = 0
+
+    team_name = html_list[index]
+    html_list = html_list[index + 1:]
+
+    # Get score
+    for i in range(len(html_list)):
+        #print(html_list[i])
+        if colour_key in html_list[i]:            
+            index = i
+            break
+        else:
+            index = 0
+            
+    score = html_list[index]
+    html_list = html_list[index + 1:]
+    
+    #print(team_name)
+    #print(score)
+    #print(html_list[index])
+    if team_key in team_name:
+        #print("Not empty")
+        team_name, score = clean_data(team_name, score, team_key, colour_key)
+        return [team_name, score], html_list
+    else:
+        #print("Empty")
+        #print("Team TBD", "Score TBD")
+        return ["Team TBD", "Score TBD"], html_list
+
+def clean_data(team_name, score, team_key, colour_key):
+    # Extract team name
+    start = team_name.find(team_key) + len(team_key)
+    team_name = team_name[start + 1:]
+    underscores = [team_name.find("_"), ""]
+    temp = team_name[underscores[0] + 1:]
+    underscores[1] = underscores[0] + temp.find("_")
+    u = underscores
+    t = team_name
+    team_name = t[:u[0]] + " " + t[u[0] + 1: u[1] + 1]
+    # Can use team name to abbreviation converter here
+
+    # Extract score
+    start = score.find(colour_key)
+    score = score[start + len(colour_key) + 4:]
+    if score[0] == "<": score = score[3]
+    else: score = score[0]
+    #print(team_name, score)
+    return team_name, score
+
 def main():
     data = []
-    playoffs_2017 = wikipedia.page("Template:2017_NBA_Playoffs")
-    playoffs_2016 = wikipedia.page("Template:2016_NBA_Playoffs")
-    #page_html = playoffs_2016.html()
-    page_html = playoffs_2017.html()
-    # Each team name links to their season page, which
-    # all begin with "2015%E2%80%9316" ("2015-2016"); use this as a key
-    #team_key = "2015%E2%80%9316"
-    team_key = "2016%E2%80%9317"
-    #html_section = page_html[page_html.find(team_key):]
+    year = 2017
+    if year == 2016:
+        playoffs_2016 = wikipedia.page("Template:2016_NBA_Playoffs")
+        page_html = playoffs_2016.html()
+        team_key = "2015%E2%80%9316"
+    elif year == 2017:
+        playoffs_2017 = wikipedia.page("Template:2017_NBA_Playoffs")
+        page_html = playoffs_2017.html()
+        team_key = "2016%E2%80%9317"
     html_section = page_html
-    #print(html_section)
-    #return
-
-    #get_results(html_section, team_key, colour_key)
-
-    # data is a list of lists
-    # Get Eastern Conference results
-    colour_key = "#87cefa"
+    start = html_section.find("<td height=\"7\">")
+    html_section = html_section[start:]
+    html_list = html_section.split("</td>")
+    east_colour_key = "#87cefa"
+    west_colour_key = "#ffaeb9"
+    #for i in html_list: print(i)
     for i in range(15):
-        d, html_section = get_results(html_section, team_key, colour_key)
+        d, html_list = get_results(html_list, team_key, east_colour_key)
         data += [d]
 
-    # Get Western Conference results
-    colour_key = "#ffaeb9"
     for i in range(15):
-        d, html_section = get_results(html_section, team_key, colour_key)
+        d, html_list = get_results(html_list, team_key, west_colour_key)
         data += [d]
-
     organise_results(data)
-    #print(data)
-    #print(data[0][0], data[0][1])
-    #print(data[1][0], data[1][1])
-    count = 0
-
-    for i in data:
-        #if count % 2 == 0: print()
-        #print(i)
-        count += 1
-        #pass
+    #for i in range(len(data)):
+     #   if i % 2 == 0 and i > 0: print()
+      #  print(data[i])
 
 
 main()
