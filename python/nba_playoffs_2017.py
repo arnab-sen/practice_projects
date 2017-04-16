001e4e592b751a959795cc4bd128b77b57ca4f54
DESKTOP-IVA7AI6\Arnab <azure3317@gmail.com>
2017-04-16 19:26:18 +1200

    Now organises matches into first round/ conference semifinals/ conference finals/ nba finals

--- python/nba_playoffs_2017.py
+++ python/nba_playoffs_2017.py
@@ -30,6 +30,47 @@ def get_results(html_section, team_key, colour_key):
 
     return [team_name, score], html_section
 
+def organise_results(data):
+    first_round = [""] * 8
+    conf_semi = [""] * 4
+    conf_finals = [""] * 2
+    nba_finals = [""]
+
+    # Group data into matches
+    matches = [""] * 15
+    match_index = 0
+    for i in range(30):
+        if i % 2 == 0:
+            matches[i // 2] = [data[i], data[i + 1]]
+    #for i in range(len(matches)): print(i, matches[i])
+
+    # Get first round matches (0, 2, 4, ... 14)
+    for i in range(15):
+        if i % 2 == 0: first_round[i // 2] = matches[i]
+
+    # Get conference semifinal matches (1, 5, 9, 13)
+    for i in range(15):
+        if i == 0: conf_semi[i] = matches[i]
+        elif (i - 1) % 4 == 0: conf_semi[(i - 1) // 4] = matches[i]
+
+    # Get conference finals matches (3, 11)
+    conf_finals[0] = matches[3]
+    conf_finals[1] = matches[11]
+
+    # Get nba finals match (7)
+    nba_finals = matches[7]
+    
+    print("First Round:")
+    for i in first_round: print(i)
+    print("\nConference Semifinals:")
+    for i in conf_semi: print(i)
+    print("\nConference Finals:")
+    for i in conf_finals: print(i)
+    print("\nNBA Finals:")
+    print(nba_finals)
+        
+            
+
 data = []
 playoffs_2017 = wikipedia.page("2017 NBA Playoffs")
 playoffs_2016 = wikipedia.page("Template:2016_NBA_Playoffs")
@@ -54,11 +95,26 @@ for i in range(15):
     d, html_section = get_results(html_section, team_key, colour_key)
     data += [d]
 
+organise_results(data)
+#print(data)
 #print(data[0][0], data[0][1])
 #print(data[1][0], data[1][1])
-for i in data: 
-    print(i)
+count = 0
+
+for i in data:
+    #if count % 2 == 0: print()
+    #print(i)
+    count += 1
     #pass
 
 
 #print(html_section)
+
+"""
+ISSUES:
+- Team names only show the first two words, which is fine for
+  e.g. Cleveland Cavaliers, but for the Clippers, their team
+  name comes up as "Los Angeles" rather than "Los Angeles
+  Clippers" -- write a function that shortens all team
+  names to their respective acronyms (e.g. CLE, LAC, GS)
+"""
