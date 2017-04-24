0778874ef641058dc6da5f989eba2d9c1938ba0c
DESKTOP-IVA7AI6\Arnab <azure3317@gmail.com>
2017-04-25 00:47:52 +1200

    The user can now input which colour they wish to remove

--- python/RGBeGone!/rgbegone.py
+++ python/RGBeGone!/rgbegone.py
@@ -7,10 +7,12 @@ def create_directories(*directories):
             os.makedirs(directory)
     
 def get_image(file_name):
+    if file_name[-4:] != ".png": file_name += ".png"
     return Image.open("Input\\" + file_name)
 
-def save_image(image, save_name):
-    image.save("Output\\" + save_name)
+def save_image(image, file_name):
+    if file_name[-4:] != ".png": file_name += ".png"
+    image.save("Output\\" + file_name)
 
 def get_RGBA(image):
     return list(image.getdata())
@@ -26,18 +28,23 @@ def change_pixels(image, new_pixel_data):
     
 def main():
     create_directories("Input", "Output")
-    file_name = input("Enter your file name (e.g. file1.png): ")
-    image = get_image(file_name).convert("RGBA")
-    pixel_data = get_RGBA(image)
-    new_pixel_data = pixel_data
-    white = (255, 255, 255)
-    for i in range(len(pixel_data)):
-        if check_pixel(pixel_data[i], white):
-            # Make pixel transparent by making the alpha
-            # channel 0
-            new_pixel_data[i] = (0, 0, 0, 0)
-    change_pixels(image, new_pixel_data)
-    save_image(image, file_name)
+    remove_colours = input("Enter the colour you wish to remove (e.g. 0 0 0): ")
+    values = remove_colours.split()
+    values = [int(i) for i in values]
+    remove_colour = (values[0], values[1], values[2])
+    file_name = input("Enter your file name: ")
+    while file_name != "exit":
+        image = get_image(file_name).convert("RGBA")
+        pixel_data = get_RGBA(image)
+        new_pixel_data = pixel_data
+        for i in range(len(pixel_data)):
+            if check_pixel(pixel_data[i], remove_colour):
+                # Make pixel transparent by making the alpha
+                # channel 0
+                new_pixel_data[i] = (0, 0, 0, 0)
+        change_pixels(image, new_pixel_data)
+        save_image(image, file_name)
+        file_name = input("Enter your file name: ")
 
 if __name__ == "__main__":
     main()
