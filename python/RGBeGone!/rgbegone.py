aeb1fa33e2ed533c494ade9a5af2e02cf26e6641
DESKTOP-IVA7AI6\Arnab <azure3317@gmail.com>
2017-04-24 19:16:36 +1200

    Created a simple tool that converts an image with a white background to an image with that white background transparent

---
+++ python/RGBeGone!/rgbegone.py
@@ -0,0 +1,42 @@
+from PIL import Image
+import os
+
+def create_directories(*directories):
+    for directory in directories:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    
+def get_image(file_name):
+    return Image.open("Input\\" + file_name)
+
+def save_image(image, save_name):
+    image.save("Output\\" + save_name)
+
+def get_RGBA(image):
+    return list(image.getdata())
+
+def check_pixel(rgba, wanted_colour):
+    if rgba[:3] == wanted_colour:
+        return True
+    else:
+        return False
+
+def change_pixels(image, new_pixel_data):
+    image.putdata(new_pixel_data)
+    
+def main():
+    create_directories("Input", "Output")
+    image = get_image("Test_01.png").convert("RGBA")
+    pixel_data = get_RGBA(image)
+    new_pixel_data = pixel_data
+    white = (255, 255, 255)
+    for i in range(len(pixel_data)):
+        if check_pixel(pixel_data[i], white):
+            # Make pixel transparent by making the alpha
+            # channel 0
+            new_pixel_data[i] = (0, 0, 0, 0)
+    change_pixels(image, new_pixel_data)
+    save_image(image, "Output_Test_01.png")
+
+if __name__ == "__main__":
+    main()
