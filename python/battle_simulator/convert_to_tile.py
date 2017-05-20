3c01940b4515cd2230ecb72c474a774b685288f3
DESKTOP-IVA7AI6\Arnab <azure3317@gmail.com>
2017-05-20 16:33:11 +1200

    Creates save directory and whole map image to that directory; scale changed to 2x for tile_viewer

--- python/battle_simulator/convert_to_tile.py
+++ python/battle_simulator/convert_to_tile.py
@@ -5,10 +5,11 @@ that image into a series of tiles, and saves the tiles to a folder.
 import os
 from PIL import Image
 TILE_SIZE_ORIGINAL = (16, 16)
-TILE_SIZE = (TILE_SIZE_ORIGINAL[0] * 3, TILE_SIZE_ORIGINAL[1] * 3)
+SCALE = 2
+TILE_SIZE = (TILE_SIZE_ORIGINAL[0] * SCALE, TILE_SIZE_ORIGINAL[1] * SCALE)
 TILE_DIM = TILE_SIZE[0]
-TEST_OPEN_PATH = "Resources\\Overworld\\Tiles\\Input\\"
-TEST_SAVE_PATH = "Resources\\Overworld\\Tiles\\Saved\\"
+TEST_OPEN_PATH = "Resources/Overworld/Tiles/Input/"
+TEST_SAVE_PATH = "Resources/Overworld/Tiles/Saved/"
 maps = {}
 
 def scale_image(scale, image):
@@ -21,10 +22,11 @@ def get_map(filename):
     image = Image.open(TEST_OPEN_PATH + filename + ".png")
     maps["current"] = filename
     maps[filename] = {}
-    image = scale_image(3, image)
+    image = scale_image(SCALE, image)
     maps[filename]["tiles per row"] = round(image.size[0] / TILE_SIZE[0])
     maps[filename]["tiles per col"] = round(image.size[1] / TILE_SIZE[1])
     maps[filename]["dimensions"] = image.size
+    maps[filename]["image"] = image
 
     return image
 
@@ -39,7 +41,6 @@ def make_tiles(image):
         for tile in range(maps[current_map]["tiles per row"]):
             crop_pos[0] = (TILE_DIM * tile)
             crop_pos[2] = crop_pos[0] + TILE_DIM
-            print(tuple(crop_pos))
             tiles.append(image.crop(crop_pos))
 
     return tiles
@@ -58,11 +59,20 @@ def split_list(cols, linear_list):
 def save_tiles(tiles):
     tiles_per_row = maps[maps["current"]]["tiles per row"]
     tiles = split_list(tiles_per_row, tiles)
+    directory = TEST_SAVE_PATH + maps["current"]
+
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # Save base image
+    base = maps[maps["current"]]["image"]
+    base.save(directory + "/" + maps["current"] + ".png")
     
+    # Save tiles
     for i in range(len(tiles)):
         for j in range(tiles_per_row):
-            name = TEST_SAVE_PATH
-            name += maps["current"] + " [{}, {}].png".format(str(i), str(j))
+            name = directory + "/"
+            name += maps["current"] + " [{}, {}].png".format(i, j)
             tiles[i][j].save(name)
 
 def main():
