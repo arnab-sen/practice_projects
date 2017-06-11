f64740d015c42d6a37a0d20bb2c599892c08f27a
DESKTOP-IVA7AI6\Arnab <azure3317@gmail.com>
2017-06-12 01:39:11 +1200

    Created a basic network monitor to log connectivity problems

---
+++ python/Network Monitor/network_monitor.py
@@ -0,0 +1,61 @@
+"""
+* Initial version: a simple tool to check and log internet connectivity,
+useful for monitoring the frequency of connection drops.
+* Connectivity based on try/except with urllib.request
+"""
+import urllib.request, datetime
+import time as TIME
+
+def get_time(date_only = None):
+    timezone_offset = 12
+    date_and_time = str(datetime.datetime.now())
+    date = date_and_time[:11]
+    time = date_and_time[11:]
+    rest = time[time.find(":"):time.rfind(".")]
+    hour = time[:time.find(":")]
+    #hour = change_hour(hour, timezone_offset)
+
+    if date_only:
+        return date
+    else:
+        return date + "-- " + hour + rest
+
+def change_hour(hour, timezone_offset):
+    hour = int(hour)
+    hour = (hour + timezone_offset) % 24
+    hour = str(hour)
+
+    if len(hour) < 2:
+        hour = "0" + hour
+
+    return hour
+
+def write_to_log(entry):
+    date = get_time(date_only = True)
+    with open(date + " Log.txt", "a") as log:
+        log.write(entry + "\n")   
+
+def run_monitor(wait_seconds):
+    url = "https://google.com"
+    while 1:
+        try:
+            with urllib.request.urlopen(url) as response:
+                packet = response.read(1)
+                
+            time = get_time()
+            entry = time + "\t" + "OK"
+            print(entry)
+            write_to_log(entry)
+        except:
+            time = get_time()
+            entry = time + "\t" + "DISCONNECTED"
+            print(entry)
+            write_to_log(entry)
+
+        TIME.sleep(wait_seconds)
+
+def main():
+    run_monitor(3)
+
+if __name__ == "__main__":
+    main()
