0905fdaff42c681adb63d89af1e7583ce31d9831
DESKTOP-IVA7AI6\Arnab <azure3317@gmail.com>
2017-06-24 22:14:48 +1200

    Added a controllable Mario

--- python/pyMario/pyMario.py
+++ python/pyMario/pyMario.py
@@ -2,11 +2,31 @@
 
 """
 import pygame
-import map_editor
+import map_editor, utils
 from PIL import Image
 
 # GLOBALS
 res = {} # A resource dictionary
+
+class Entity:
+    def __init__(self, name, path):
+        self.name = name
+        self.path = path
+        self.load_sprites()
+        self.postion = [0, 0]
+
+    def load_sprites(self):
+        self.sprite_png = Image.open(self.path + "/" + self.name + ".png")
+        self.sprite_png = utils.scale_image(res["scale"], self.sprite_png)
+        self.sprite_pygame = utils.image_to_pygame(self.sprite_png)
+
+class Mario(Entity):
+    def __init__(self, name, path):
+        super().__init__(name, path)
+
+    def jump(self):
+        self.position[1] -= 1
+    
 
 def set_current_map():
     map_name = "1_1"
@@ -16,11 +36,19 @@ def set_current_map():
 def load_map(map_name):
     pass
 
+def load_character():
+    path = "Resources/Images/Playable"
+    name = "mario"
+    res["mario"] = Mario(name, path)
+
+    return res["mario"]
+
 def main():
     pass
 
 def test_main():
     image_scale = 2
+    res["scale"] = image_scale
     test_map = map_editor.Map(scale = image_scale)
     pygame.init()
     #screen_size = (1280, 720)
@@ -29,8 +57,11 @@ def test_main():
     pygame.display.set_caption("pyMario")
     window_icon = pygame.image.load("Resources/Images/Other/pyMario_icon.png")
     pygame.display.set_icon(window_icon)
-    map_position = [-5000, 0]
+    map_position = [0, 0] #[-5000, 0] for near the end
     move_pixels = round(2.5 * image_scale)
+    mario = load_character()
+    mario.position = [screen_size[0] // 2, screen_size[1] - 80]
+    
     while 1:
         keys = pygame.key.get_pressed()
         for event in pygame.event.get():
@@ -45,9 +76,12 @@ def test_main():
             at_left_border = -1 * map_position[0] < 0
             if not at_left_border:
                 map_position[0] += move_pixels
+        elif keys[pygame.K_UP]:
+            mario.jump()
             
         
         screen.blit(test_map.image_pygame, map_position)
+        screen.blit(mario.sprite_pygame, mario.position)
         pygame.display.flip()
 
 def TODO():
