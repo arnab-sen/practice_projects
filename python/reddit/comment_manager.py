022cd59d006a2e27cec381ee9a9da1c977c2c6c1
DESKTOP-IVA7AI6\Arnab <azure3317@gmail.com>
2017-08-19 16:46:14 +1200

    v1.0 - Current features only include comment editing and deletion

---
+++ python/reddit/comment_manager.py
@@ -0,0 +1,40 @@
+import praw
+
+class User():
+	def __init__(self):
+		self.__username, self.reddit = self.__get_auth_instance()
+		self.__user = self.reddit.redditor(self.__username)
+
+	def __get_auth_instance(self):
+
+		with open("auth.txt", "r") as file:
+			u, p, c_id, c_secret = file.read().split()
+		
+		reddit = praw.Reddit(
+								client_id = c_id,
+								client_secret = c_secret,
+								username = u,
+								password = p,
+								user_agent = "My Comment Manager"
+								)
+
+		return u, reddit
+
+	def print_comments(self):
+		for comment in self.__user.comments.new():
+			print(comment.body + "\n")
+
+	def get_username(self):
+		return self.__username
+
+	def get_comments(self):
+		return list(self.__user.comments.new())
+
+def main():
+	user = User()
+	comments = user.get_comments()
+	for comment in comments:
+		comment.edit("[deleted]")
+		comment.delete()
+
+main()
